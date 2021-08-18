78203e10297877a820ffc0d91a9a3e57cbcf6eb4
uuutah <78704588+uuutah@users.noreply.github.com>
2021-08-18 21:33:58 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :shopping_charges, :shopping_area, :days_to_ship
 end
