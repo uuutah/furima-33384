4c4708a690e056a577ac8d4eeeeadb730013c4d0
uuutah <78704588+uuutah@users.noreply.github.com>
2021-08-19 11:13:02 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,16 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :status, :shopping_charges, :shopping_area, :days_to_ship
+
+  with_options presence: true, message: "can't be blank" do
+    validates :title, :image, :description, :category, :days_to_ship, :shopping_charges, :shopping_area,
+              :status, :price
+  end
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category, :days_to_ship, :shopping_charges, :shopping_area, :status
+  end
+
+  validates :price, format: { with: /\A[0-9]+\z/ }, length: { maximum: 299, minimum: 10000000 }
+
 end
